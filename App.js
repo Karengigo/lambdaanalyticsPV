69abab8540f80808c4ce86952125a1d868391f72
Lambda Analytics <114586895+Karengigo@users.noreply.github.com>
2022-09-28 07:58:05 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const envio=()=>{
     geotab.addin.integrationExample = function(api, state) {
     const BotonEnvio=()=>{
     const div=document.getElementById("envio")
-    div.innerHTML+=`<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZmU5NmMwM2EtYTQ2MS00MTVjLWE1YmItZWY1OGQzM2ZiOTFiIiwidCI6IjFlNDFhMGVmLTQ4MDctNGVlNS1hZjk0LTYwZjJkMDNjMzRlYSIsImMiOjR9&pageName=ReportSectionf46a0a018606010b0595" frameborder="0" allowFullScreen="true"></iframe>`;
+    div.innerHTML+=`<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMmY3OTRlYzQtZDMwZC00MjI2LTlkOTYtZTlmOGNkZjk3MTkyIiwidCI6Ijg5ODA1OTQ2LTEzN2UtNDhjOS1hYzE1LWQ2NDM0YTE5ODkyNiJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>`;
     }
     const EliminacionBoton=()=>{
         const div=document.getElementById("envio")
